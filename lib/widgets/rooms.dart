374c4892b036270e9cc0328a5b1b03e09faf91c7
Karan Vaswani <karanvaswani2002@gmail.com>
2021-08-06 13:03:37 +0530

    Facebook UI clone ||Responsive||

---
+++ lib/widgets/rooms.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  const Rooms({Key key, @required this.onlineUsers}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+          : null,
+      child: Container(
+        height: 60.0,
+        color: Colors.white,
+        child: ListView.builder(
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4.0),
+            scrollDirection: Axis.horizontal,
+            itemCount: 1 + onlineUsers.length,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: _CreateRoomButton(),
+                );
+              }
+              final User user = onlineUsers[index-1];
+              return Padding(padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ProfileAvatar(
+                imageUrl: user.imageUrl,
+                isActive: true
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}
+
+class _CreateRoomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () => print('Create Room'),
+      style: OutlinedButton.styleFrom(
+          side: BorderSide(
+              width: 3.0,
+              color: Colors.blueAccent[100]
+          ),
+          primary: Palette.facebookBlue,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0))),
+      child: Row(
+        children: [
+         // ShaderMask(
+           // shaderCallback: (rect)=> Palette.createRoomGradient.createShader(rect),
+            //child: Icon(Icons.video_call,
+             // size: 35.0,
+              //color: Colors.white,
+           // ),
+         // ),
+          Icon(Icons.video_call,
+            size: 35.0,
+            color: Colors.purple,
+          ),
+          const SizedBox(height: 4.0,),
+          Text('Create\nRoom'),
+        ],
+      ),
+    );
+  }
+}
